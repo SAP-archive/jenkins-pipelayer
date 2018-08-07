7bdb568e30939791a68355507196d7d6a09a9029
Frederic ROUSSEAU <frederic.rousseau@sap.com>
2018-08-07 10:11:55 +0200

    Update processTemplates.groovy

--- vars/processTemplates.groovy
+++ vars/processTemplates.groovy
@@ -25,7 +25,7 @@ def call(String path, commit) {
             def filePath = properties['jenkins.job.template']
             def fileName = properties['jenkins.job.name']
             if (!fileName) {
-                fileName = file.replaceFirst(~/\.[^\.]+$/, '').split('/')[-1]
+                fileName = fileName.replaceFirst(~/\.[^\.]+$/, '').split('/')[-1]
             }
             arrFiles <<  [
                 // Template path
