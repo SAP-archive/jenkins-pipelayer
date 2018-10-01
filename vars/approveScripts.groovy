5d2ddbe66e6d54d16569db6d3829cdb0963d5264
i301937 <frederic.rousseau@sap.com>
2018-10-01 13:29:53 +0200

    fix jenkins url

--- vars/approveScripts.groovy
+++ vars/approveScripts.groovy
@@ -1,35 +1,34 @@
+#!/usr/bin/env groovy
 
-
-def runRemoteScript(credentialId, scriptText) {
+def runRemoteScript(credentialId, scriptText, jenkinsContext) {
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: credentialId,
                 usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_PASSWORD']]) {
-    def CRUMB = sh(returnStdout: true, script: """
-        curl -k -u '${JENKINS_USER}':'${JENKINS_PASSWORD}' '${env.JENKINS_URL}crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
-    """).trim()
-    sh """
-        curl -k  --user '${JENKINS_USER}':'${JENKINS_PASSWORD}' -H "${CRUMB}" --data-urlencode "script=${scriptText}" ${env.JENKINS_URL}scriptText
-    """
-}
+        def jenkinsUrl = jenkinsContext.env.JENKINS_URL
+        def CRUMB = sh(returnStdout: true, script: """
+            curl -k -u '${JENKINS_USER}':'${JENKINS_PASSWORD}' '${jenkinsUrl}crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
+        """).trim()
+        sh """
+            curl -k  --user '${JENKINS_USER}':'${JENKINS_PASSWORD}' -H "${CRUMB}" --data-urlencode "script=${scriptText}" ${jenkinsUrl}scriptText
+        """
+    }
 }
 
-def call(credentialId) {
+def call(credentialId, jenkinsContext) {
     def scriptText = '''
 import org.jenkinsci.plugins.scriptsecurity.scripts.*
 
-ScriptApproval sa = ScriptApproval.get();
+ScriptApproval sa = ScriptApproval.get()
 
 // approve scripts
 for (ScriptApproval.PendingScript pending : sa.getPendingScripts()) {
-    sa.approveScript(pending.getHash());
-    println "Approved : " + pending.script
+    sa.approveScript(pending.getHash())
 }
 
 // approve signatures
 for (ScriptApproval.PendingSignature pending : sa.getPendingSignatures()) {
-    sa.approveSignature(pending.signature);
-    println "Approved : " + pending.signature
+    sa.approveSignature(pending.signature)
 }
     '''
-    runRemoteScript(credentialId, scriptText)
+    runRemoteScript(credentialId, scriptText, jenkinsContext)
 }
 return this
